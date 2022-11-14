608616f66ec2c6fa6fecd16656fd36dae69afbe6
usertheking <ulugbekbekkarimov@gmail.com>
2022-11-14 19:42:53 +0300

    nulish, if else, ternary operator, data conversion

---
+++ terminal.js
@@ -0,0 +1,177 @@
+
+// let a = "2";
+
+
+// console.log(a);
+// console.log(Number.parseInt(a))
+// console.log(Number.parseFloat(a));
+// console.log(Number(a));
+
+// let a = "s";
+// console.log(Number(a));
+
+// console.log(false == 0);
+
+// console.log(true == 1);
+
+// console.log(typeof true == typeof null);
+// console.log(false == null);
+
+// console.log(undefined == null);
+
+
+
+// console.log(undefined == false);
+
+
+
+// console.log(true == "1");
+// console.log(true == "a");
+// console.log(true == "2");
+
+// let a = 50;
+
+// a += 10; 
+
+// console.log(a);
+
+// let b = 20;
+
+// b *= 10;
+
+// console.log(b);
+
+// let c = 30;
+
+// c /= 3;
+
+// console.log(c);
+
+
+// let name = "Ilya";
+
+// console.log(`Hello ${name}`);
+
+
+
+// let b = 1;
+// let d = ++b;
+// console.log(d);
+// console.log(d);
+
+// console.log(null + 1); 
+
+
+
+// console.log(2 * (2 + 1) / 2);
+
+
+// let a = null;
+// console.log(a + 1);
+// console.log(a);
+
+// console.log(true == 1);
+// console.log(true === 1);
+
+// console.log(true !== false);
+// console.log(true != false);
+
+
+
+// console.log(1 !== "0");
+
+// console.log(1 !== "0" || !false);
+
+// console.log(1 == "0" || false);
+
+
+
+// console.log(1 == "0" || (false && "hey"));
+// console.log(1 == "0" || (true && "hey"));
+// console.log(1 !== "0" && false && "hey");
+
+// console.log(1 !== "0" && true && "hey");
+
+
+// console.log(!(1 !== "0" && true && "hey"));
+
+
+// console.log(!"hey");
+
+
+// let temp = 1; 
+
+
+// console.log("Havo issiq");
+// console.log("Havo sovuq");
+
+// ====================(if else with ||)
+
+// let  temp = -2; 
+
+// if(temp < 0){
+//     console.log("very cold");
+// }else if(temp <= 0 || temp < 10){
+//     console.log("cold");
+// }else if(temp <= 10 || temp < 15){
+//     console.log("cool");
+// }else if(temp <= 15 || temp < 20){
+//     console.log("warm");
+// }else if(temp <= 20 || temp < 25){
+//     console.log("hot");
+// }else{
+//     console.log("boiling");
+// }
+
+// ============== (if else with &&)
+
+// let temp = 28;
+
+// if(temp <= 0){
+//     console.log("very cold");
+// }else if(temp > 0 && temp < 10){
+//     console.log("cold");
+// }else if(temp >= 10 && temp < 15){
+//     console.log("cool");
+// }else if(temp >= 15 && temp < 20){
+//     console.log("warm weather");
+// }else if(temp >= 20 && temp < 25){
+//     console.log("hot");
+// }else{
+//     console.log("boiling");
+// }
+
+
+
+
+
+
+// let temp = 3;
+// if (temp <= 2) {
+//     console.log("havo sovuq");
+// }
+// else if (temp > 2) console.log("havo yaxshi");  
+
+
+
+
+// ============ "ternary operator"
+
+// let temp = 5;
+
+
+// temp < 0 
+// ? console.log("havo good") 
+// : temp < 10
+// ? console.log("good")
+// : temp < 15
+// ? console.log("hot")
+// : console.log("boiling");
+
+
+// ==========(Nulish ?? => null, undefined)
+
+// console.log(false || 1);
+// console.log(false ?? 1);
+
+
