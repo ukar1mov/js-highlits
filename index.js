334126934ca85ddfa764dd89f47324a11f3aae2f
usertheking <ulugbekbekkarimov@gmail.com>
2022-11-27 08:07:03 +0000

    highlights

---
+++ index.js
@@ -0,0 +1,178 @@
+
+// const array = [1, 2, 3]
+
+// function addElementToArray(element){
+//     array.push(element)
+// }
+
+
+
+// function square(number){
+//     return number * number;
+// }
+// console.log(square(5));
+
+// ==========================function min(a, b)
+// function min(a,b){
+//     if(a < b){
+//         console.log(a);
+//     }
+//     else if(a == b){
+//         console.log("equals");
+//     }
+//     else{
+//         console.log(b);
+//     }
+// }
+
+// min(9, 6)
+
+// ==============================="?" function
+
+
+// function checkAge(age){
+//     (age) ?
+
+    
+// }
+
+// ================================= pow(x, n)
+// function pow(x, n){
+//     var z = 1;
+//     for(i = 1; i <= n; i++){
+//         z = z * x;
+//     }
+//     console.log(z);
+// }
+
+// pow(2, 3)
+
+// ===========================
+
+// let a = 4;
+// for (i = 10; i <= 1; i--){
+//     console.log(i);
+// }
+
+
+// let a = 'world';
+
+// for (let i = a.length; i >= 0; i--){
+
+//     console.log(a[i]);
+// }
+
+
+
+// function numbers(a, num){
+//     for (i = a.length; i <= 0; i++)
+//     console.log(a[i]);
+// }
+
+
+// numbers([1, 1, 1], 10)
+
+
+
+// function tribonacci(signature, n){
+//     let res = signature.splice(0, n);
+//     for (let i = 0; i < n - 3; i++){
+//         let tripl = res[i] + res[i + 1] + [i + 2];
+//         res.push(tripl);
+//     }
+//     console.log(res);
+// }
+
+
+
+// let solution = 'world';
+// function solution(a){
+//     for (let i = 0; i >= a.length; i--){
+//     }
+//     console.log(a[i]);
+// }
+// solution(a)
+
+// function name(a, b){
+//     if(a % 2 == 0){
+//         console.log(a);
+//     }else{
+//         console.log(b);
+//     }
+// }
+
+// name(5, 5)
+// let a = 3;
+// if (a % 2 == 0){
+//     console.log('even');
+// }else{
+//     console.log('odd');
+// }
+
+
+
+// let num = 0;
+
+// for (let i = 0; i <= -num; i--) {
+//     console.log(num);
+// }
+
+// if(num > 0){
+//         console.log(-num);
+// }else{
+//     console.log(num);
+// }
+// let num = 0;
+// (num > 0)? console.log(-num) : console.log(num);
+// let a = -4;
+
+// if (a > 0){
+//     for(i = 0; i >= -a; i--){
+//         console.log(i);
+//     }
+// }else{
+//     for(i = -a; i <= a; i++){
+//         console.log(i);
+//     }
+// }
+
+
+// let a = -0.888888;
+
+// if(a > 0){
+//     console.log(a * -1);
+// }else{
+//     console.log(a * -1);
+// }
+// let = 'Uzbekistan'
+// function str(){
+    
+// }
+
+
+// let a = 2;
+// console.log(10 ^ 2);
+
+
+// let a = 23;
+// toString(a)
+// console.log(a);
+// ==============================
+
+// function personAge(){
+//     console.log();
+// }
+
+// personAge([{bill:12}, {sam:34, childAge: 2}, {mark:24}, {tom:43}]);
+
+
+
+// function key(a){
+//     console.log([1, 0][a]);
+// }   
+
+// key(a)
+
+
+
+
