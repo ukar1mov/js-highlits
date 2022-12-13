0eece9eee24464b36adb7b2e3f148681a6682227
usertheking <ulugbekbekkarimov@gmail.com>
2022-12-13 14:35:50 +0500

    functions

---
+++ functions/index.js
@@ -0,0 +1,67 @@
+// alert()
+
+
+// function getAvr(){
+//     console.log('Ulugbek');
+//     console.log('Ulugbek');
+//     console.log('Ulugbek');
+//     console.log('Ulugbek');
+//     console.log('Ulugbek');
+//     console.log('Ulugbek');
+//     console.log('U');
+// }
+// getAvr();
+
+
+// getAvr();
+//========= Hoisting
+// getAvr();
+
+// function getAvr(){
+//     console.log("Ulug'bek, welcome to JS!");
+// }
+
+//========function expression
+// const getAvr = function(){
+//     console.log("Ulug'bek, welcome to JS!");
+// }
+//     getAvr();
+
+
+// var getAvr = function(){
+//     console.log('Ulugbek, welcome to webbrain!');
+// }
+
+// getAvr();
+
+//===========Js parameters
+// const getAvr = function(name) {
+//     console.log(`${name}, welcome to JS parameters!`);   
+// }
+
+// getAvr(`Ulug'bek`);
+// getAvr(`Karimov Ulug'bek`);
+// getAvr(`Karimov Ulug'bek Fayzulla o'g'li`)
+
+// const getAvr = function (name, surname, age){
+//     console.log(`Name:${name}`);
+//     console.log(`Surname:${surname}`);
+//     console.log(`Age:${age}`);
+// }
+// getAvr(`Ulug'bek`, `Karimov`, 18);
+//=========inner variable
+// const test = ()=>{
+//     let name = "webbrain";
+//     console.log(name);
+// }
+
+// test();
+//=========outer variable
+// let name = "Ulug'bek"
+// const test = ()=>{
+//     console.log(name);
+// }
+
+// test();
+
+//===========shadow variable
